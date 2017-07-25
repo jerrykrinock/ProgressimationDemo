b11a441b2af81dad38e83072780744664fd0fe63
Jerry Krinock <jerry@sheepsystems.com>
2017-07-25 13:37:52 -0700

    Initial Commit

---
+++ ProgressimationDemo/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ProgressimationDemo
+//
+//  Created by Jerry on 17/07/25.
+//  Copyright © 2017 Jerry. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ ProgressimationDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ProgressimationDemo
+//
+//  Created by Jerry on 17/07/25.
+//  Copyright © 2017 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
